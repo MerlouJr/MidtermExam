f7229ba006b1d2f05507560dbadebf18e9b7e9b1
MerlouJr <spec2gunz2@gmail.com>
2017-02-12 14:10:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cosain/midtermexam/adapters/NoteAdapter.java
@@ -0,0 +1,53 @@
+package com.example.cosain.midtermexam.adapters;
+
+/**
+ * Created by cosain on 2/12/2017.
+ */
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.cosain.midtermexam.R;
+import com.example.cosain.midtermexam.models.Note;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends ArrayAdapter<Note>{
+    private static class ViewHolder {
+        TextView id;
+        TextView title;
+    }
+
+    public NoteAdapter(Context context, ArrayList<Note> notes) {
+        super(context, R.layout.item_note, notes);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Note note = getItem(position);
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_note, parent, false);
+
+            viewHolder.id = (TextView) convertView.findViewById(R.id.value_note_id);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.value_note_title);
+
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.id.setText(note.getId());
+        viewHolder.title.setText(note.getTitle());
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/cosain/midtermexam/clients/NoteRestClient.java
@@ -0,0 +1,27 @@
+package com.example.cosain.midtermexam.clients;
+
+
+/**
+ * Created by cosain on 2/12/2017.
+ */
+import android.content.Context;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import cz.msebera.android.httpclient.Header;
+public class NoteRestClient {
+    private static final String BASE_URL = "http://ws.audioscrobbler.com/2.0/";
+
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(Context context, String url, Header[] headers, RequestParams params,
+                           AsyncHttpResponseHandler responseHandler) {
+        client.get(context, getAbsoluteUrl(url), headers, params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
